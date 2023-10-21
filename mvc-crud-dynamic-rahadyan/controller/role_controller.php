da8a03b73ed077f5554caaea36aed6c3bc1f3678
RahadyanRizqy <rahadyanrizqy@gmail.com>
2023-10-21 17:07:31 +0700

    up dir 1

---
+++ mvc-crud-dynamic-rahadyan/controller/role_controller.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'controller/controllers.php';
+class RoleController{
+    static function index() {
+        view('subview/roles', [
+            'roles' => Roles::select(),
+            'header' => titleheader('Read data scr AJAX dgn jQuery', 'h1', 'text-center mb-3')
+        ]);
+    }
+
+    static function getRoles($id) {} # Ditugaskan
+}
